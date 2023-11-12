c42077a8c584f54eb423967b95e8e652954a142c
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-12 17:52:40 +0000

    create schedule dynamically - WIP

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 var currentDayElement = $('#currentDay');
 var todaysDate = dayjs().format('dddd DD MMMM YYYY');
 currentDayElement.text(todaysDate);
+
+var rowContainer = $('.container');
+
+var currentHour = dayjs().format('HH');
+// console.log(currentHour);
+
+// dynamically create schedule rows
+
+function buildSchedule() {
+    
+}
