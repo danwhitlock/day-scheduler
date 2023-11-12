3725ee1edbb0a1944d957f3972a6ff9619d6f48c
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-12 18:59:58 +0000

    create schedule dynamically

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var currentDayElement = $('#currentDay');
 var todaysDate = dayjs().format('dddd DD MMMM YYYY');
+
 currentDayElement.text(todaysDate);
 
 var rowContainer = $('.container');
@@ -7,8 +8,33 @@ var rowContainer = $('.container');
 var currentHour = dayjs().format('HH');
 // console.log(currentHour);
 
-// dynamically create schedule rows
-
-function buildSchedule() {
+// dynamically create a schedule row
+function buildRow() {
+    // create and append the row
+    var plannerRow = $('<div>');
+    plannerRow.addClass('row');
+    rowContainer.append(plannerRow);
     
-}
+    // create and append the hour box
+    var hourBox = $('<div>');
+    hourBox.addClass('hour');
+    plannerRow.append(hourBox);
+
+    // create and append the appointment box
+    var apptBox = $('<div>');
+    apptBox.addClass('time-block');
+    plannerRow.append(apptBox);
+
+    // create and append the save button
+    var saveButton = $('<div>');
+    saveButton.addClass('saveBtn');
+    plannerRow.append(saveButton);
+};
+
+buildRow();
+
+
+// loop to create 9 rows
+for (let i = 0; i < 8; i++) {
+    buildRow();
+};
